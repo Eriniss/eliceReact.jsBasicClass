e80ff300006075c3fa0775fd544b24873c1398d6
Eriniss <jhs2195@gmail.com>
2022-06-25 00:05:09 +0900

    addREADME

--- chapter3/chapter3.js
+++ chapter3/chapter3.js
@@ -1,11 +1,7 @@
 const arr = [1, 2, 3, 4, 5];
 
 const myFunc = (...n) => {
-  let sum = 0;
-  n.map((item) => {
-    sum += item;
-  })
-  return sum;
+  return arr + 2;
 }
 
 console.log(myFunc(...arr));
