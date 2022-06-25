f33f8f1801899d786838d554fdc4a54369fb7011
Eriniss <jhs2195@gmail.com>
2022-06-25 09:14:11 +0900

    3.1

--- chapter3/chapter3.js
+++ chapter3/chapter3.js
@@ -1,7 +1,15 @@
-const arr = [1, 2, 3, 4, 5];
+// * Create React App 소개
 
-const myFunc = (...n) => {
-  return arr + 2;
-}
 
-console.log(myFunc(...arr));
+// CRA란?
+// React 프로젝트를 손쉽게 생성할 수 있도록 도와주는 '보일러플레이트'
+// 수많은 React용 보일러플레이트가 있지만 Facebook에서 직접 만들어서 관리하는 Create React App이 가장 많이 쓰임
+// 프로젝트 생성에 필요한 다양한 기능을 Command로 제공함
+
+
+// CRA의 장점
+// 1. 개발자가 온전히 React App 개발에 집중할 수 있도록 함
+// 2. 모든 브라우저에서 해석될 수 있도록 transcompile 지원
+
+
+// 
