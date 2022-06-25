654b27f7f85fa5bf6e85694620879f49ba56ff60
Eriniss <jhs2195@gmail.com>
2022-06-25 21:56:20 +0900

    2.1

---
+++ chapter1.4/chapter1.4.js
@@ -0,0 +1,31 @@
+// * JSX
+
+
+// JSX란
+// JSX는 함수 호출과 객체 생성을 위한 문법적 편의를 제공하는 JS의 확장
+// HTML과 비슷하게 생겼으나 JS이고 HTML과 다른 부분이 있음
+
+
+// JSX는 Babel에 의해서 Transcompile 됨
+
+
+// JSX 특징/HTML과의 차이점
+// 1. HTML 태그 내에 JS 연산 가능
+// 2. class -> className
+// 3. 스타일은 object로
+// 4. 닫는 태그 필수
+// 5. 최상단 element는 반드시 하나
+
+
+// component란?
+// 1. React에서 페이지를 구성하는 최소단위
+// 2. Component의 이름은 반드시 대문자로 시작
+// 3. Class Component/Function Component로 나뉨
+// 4. Constrolled Component/Uncontrolled Component
+
+
+// Component의 특징
+// 1. 컴포넌트끼리 데이터를 주고받을 땐 Props
+// 2. 컴포넌트 내에서 데이터를 관리할 땐 State
+// 3. 데이터는 부모 -> 자식으로만 전달
+
